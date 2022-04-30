3162da395439328d2061ce62d9c23125e993b2b7
vanimittal1304 <vani9045060450@gmail.com>
2022-04-30 11:20:44 +0530

    system

---
+++ partials/_nav.php
@@ -0,0 +1,40 @@
+<?php 
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
+  $loggedin= true;
+}
+else{
+  $loggedin = false;
+}
+echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/php-projects/login-registration-system">iSecure</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/php-projects/login-registration-system/welcome.php">Home <span class="sr-only">(current)</span></a>
+      </li>';
+
+      if(!$loggedin){
+      echo '<li class="nav-item">
+        <a class="nav-link" href="/php-projects/login-registration-system/login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/php-projects/login-registration-system/signup.php">Signup</a>
+      </li>';
+      }
+      if($loggedin){
+      echo '<li class="nav-item">
+        <a class="nav-link" href="/php-projects/login-registration-system/logout.php">Logout</a>
+      </li>';
+      }
+       
+      
+    echo '</ul>
+    
+    </form>
+  </div>
+</nav>';
+?>
